e0fa1ac4b86946697ea2c66124d50b361210d73e
Will K <willkman718@pm.me>
2023-08-08 13:37:47 -0700

    cleaned up fileparser

--- tools/fileparser.go
+++ tools/fileparser.go
@@ -3,21 +3,13 @@ package tools
 import (
 	"fmt"
 	"io/ioutil"
-
-	//	"path/filepath"
 	"strings"
 
 	"gopkg.in/yaml.v2"
 )
 
 type FileParserConfig struct {
-	Files []struct {
-		PathToFile           string   `yaml:"pathtofile"`
-		Keywords             []string `yaml:"keywords"`
-		ParseAll             bool     `yaml:"parseAll"`
-		ParsingLevel         string   `yaml:"parsingLevel"`
-		SanitizationKeywords []string `yaml:"sanitizationKeywords"`
-	} `yaml:"files"`
+	Files []FileConfig `yaml:"files"`
 }
 
 type FileConfig struct {
@@ -26,26 +18,19 @@ type FileConfig struct {
 	ParseAll             bool     `yaml:"parseAll"`
 	ParsingLevel         string   `yaml:"parsingLevel"`
 	SanitizationKeywords []string `yaml:"sanitizationKeywords"`
+	MonitorTag           string   `yaml:"monitor_tag"`
 }
 
-// func FileParserFromYAMLConfigServer(configFilePath string) error {
 func FileParserFromYAMLConfigServer(configFilePath string, outputJSONFilePath string) error {
-	//	fmt.Println("I'm the fileParser config server")
-	// Read and parse the YAML configuration file
 	config, err := readYAMLConfig(configFilePath)
 	if err != nil {
 		return fmt.Errorf("error reading YAML config: %w", err)
 	}
 
-	// Parse each file according to the configuration
 	for _, file := range config.Files {
-		// Access the "PathToFile" field in the struct
 		filePath := file.PathToFile
-
-		// Check if parsingLevel is "server"
 		if file.ParsingLevel == "server" {
-			// Parse at the server level
-			if err := parseAtServerLevel(filePath, file.Keywords, file.SanitizationKeywords, file.ParseAll, ""); err != nil {
+			if err := parseAndAppendAtServerLevel(filePath, file, outputJSONFilePath); err != nil {
 				return err
 			}
 		}
@@ -54,127 +39,69 @@ func FileParserFromYAMLConfigServer(configFilePath string, outputJSONFilePath st
 	return nil
 }
 
-// Separate function for instance-level parsing
 func FileParserFromYAMLConfigInstance(configFilePath, outputFilePath, instance string) error {
-	//	fmt.Println("I'm the fileParser config instance")
-	// Read and parse the YAML configuration file
 	config, err := readYAMLConfig(configFilePath)
 	if err != nil {
 		return fmt.Errorf("error reading YAML config: %w", err)
 	}
 
-	// Parse each file according to the configuration
 	for _, file := range config.Files {
-		// Access the "PathToFile" field in the struct
 		filePath := file.PathToFile
-
-		// Check if parsingLevel is "instance"
 		if file.ParsingLevel == "instance" {
-			// Replace the "%INSTANCE%" placeholder with the actual instance name
 			filePath = strings.Replace(filePath, "%INSTANCE%", instance, 1)
-			// Parse at the instance level
-			if err := parseAtInstanceLevel(filePath, file.Keywords, file.SanitizationKeywords, file.ParseAll, outputFilePath, instance); err != nil {
+			if err := parseAndAppendAtInstanceLevel(filePath, file, outputFilePath, instance); err != nil {
 				return err
 			}
 		}
 	}
+
 	return nil
 }
 
-func parseAtServerLevel(filePath string, keywords, sanitizationKeywords []string, parseAll bool, outputFilePath string) error {
-	// Read the content of the file
-	fileContent, err := ioutil.ReadFile(filePath)
+func parseAndAppendAtServerLevel(filePath string, fileConfig FileConfig, outputFilePath string) error {
+	parsedContent, err := parseContent(filePath, fileConfig)
 	if err != nil {
-		return fmt.Errorf("failed to read file %q: %w", filePath, err)
-	}
-
-	// Convert the content to a string
-	content := string(fileContent)
-
-	// Process all lines if parseAll is true and keywords is empty
-	if len(keywords) == 0 && parseAll {
-		//		fmt.Println("Server Level Output for", filePath)
-		//fmt.Println(content)
-		return nil
-	}
-
-	// Process the lines containing the keywords and print them
-	// fmt.Println("Server Level Output for", filePath)
-	var outputLines []string
-	lines := strings.Split(content, "\n")
-	for _, line := range lines {
-		for _, keyword := range keywords {
-			if strings.Contains(line, keyword) {
-				// Print the line (you can perform additional operations here if needed)
-				//fmt.Println(line)
-				outputLines = append(outputLines, line)
-				break // Move to the next line after processing the current keyword
-			}
-		}
-	}
-	// Sanitize the output before printing it
-	//sanitizedOutput := sanitizeOutput(strings.Join(outputLines, "\n"), sanitizationKeywords)
-
-	// Print the sanitized output
-	//fmt.Println(sanitizedOutput)
-	//fmt.Println("Based:", EncodeToBase64(sanitizedOutput))
-	// Call the function to append parsed data for server level
-	if err := AppendServerParsedData(filePath, keywords, sanitizationKeywords, parseAll, outputFilePath); err != nil {
 		return err
 	}
-	return nil
+
+	return appendParsedData(filePath, parsedContent, fileConfig, outputFilePath)
 }
 
-func parseAtInstanceLevel(filePath string, keywords, sanitizationKeywords []string, parseAll bool, outputFilePath, instanceArg string) error {
-	// Read the content of the file
+func parseAndAppendAtInstanceLevel(filePath string, fileConfig FileConfig, outputFilePath, instanceArg string) error {
+	parsedContent, err := parseContent(filePath, fileConfig)
+	if err != nil {
+		return err
+	}
+
+	return appendParsedData(filePath, parsedContent, fileConfig, outputFilePath)
+}
+
+func parseContent(filePath string, fileConfig FileConfig) (string, error) {
 	fileContent, err := ioutil.ReadFile(filePath)
 	if err != nil {
-		return fmt.Errorf("failed to read file %q: %w", filePath, err)
+		return "", fmt.Errorf("failed to read file %q: %w", filePath, err)
 	}
-
-	// Convert the content to a string
 	content := string(fileContent)
 
-	// Process all lines if parseAll is true and keywords is empty
-	if len(keywords) == 0 && parseAll {
-		//fmt.Println("Instance Level Output for", filePath)
-		//fmt.Println(content)
-		return nil
+	// If ParseAll is true, return the full content (but still sanitize if needed)
+	if fileConfig.ParseAll {
+		return sanitizeOutput(content, fileConfig.SanitizationKeywords), nil
 	}
 
-	// Process the lines containing the keywords and print them
-	//fmt.Println("Instance Level Output for", filePath)
 	var outputLines []string
 	lines := strings.Split(content, "\n")
 	for _, line := range lines {
-		for _, keyword := range keywords {
+		for _, keyword := range fileConfig.Keywords {
 			if strings.Contains(line, keyword) {
-				// Print the line (you can perform additional operations here if needed)
-				//fmt.Println(line)
 				outputLines = append(outputLines, line)
-				break // Move to the next line after processing the current keyword
+				break
 			}
 		}
 	}
 
-	// Sanitize the output before printing it
-	//sanitizedOutput := sanitizeOutput(strings.Join(outputLines, "\n"), sanitizationKeywords)
-
-	// Print the sanitized output
-	//fmt.Println("Sanitized Output:")
-	//fmt.Println(sanitizedOutput)
-	//fmt.Println("Based:", EncodeToBase64(sanitizedOutput))
-	// Call the function to append parsed data for instance level
-	if err := AppendInstanceParsedData(filePath, keywords, sanitizationKeywords, parseAll, outputFilePath, instanceArg); err != nil {
-		return err
-	}
-	return nil
+	return sanitizeOutput(strings.Join(outputLines, "\n"), fileConfig.SanitizationKeywords), nil
 }
-
-// Sanitize the output as needed before appending to the output file.
 func sanitizeOutput(output string, sanitizationKeywords []string) string {
-	// Add your sanitization logic here.
-	// For example, you can remove lines containing the sanitization keywords from the output.
 	var sanitizedOutputLines []string
 	lines := strings.Split(output, "\n")
 	for _, line := range lines {
@@ -193,154 +120,28 @@ func sanitizeOutput(output string, sanitizationKeywords []string) string {
 }
 
 func readYAMLConfig(configFilePath string) (*FileParserConfig, error) {
-	// Print the absolute path of the config file for debugging
-	/*	absConfigPath, err := filepath.Abs(configFilePath)
-		if err != nil {
-			return nil, fmt.Errorf("failed to get absolute path of config file: %w", err)
-		}
-		fmt.Println("Absolute path of the config file:", absConfigPath)
-	*/
-	// Read the content of the YAML configuration file
 	content, err := ioutil.ReadFile(configFilePath)
 	if err != nil {
 		return nil, fmt.Errorf("failed to read YAML config file: %w", err)
 	}
 
-	// Parse the YAML content into the FileParserConfig struct
 	var config FileParserConfig
 	if err := yaml.Unmarshal(content, &config); err != nil {
-		return nil, fmt.Errorf("failed to unmarshal YAML config: %w", err)
-	}
-
-	// Ensure that all necessary fields are provided in the YAML config
-	if len(config.Files) == 0 {
-		return nil, fmt.Errorf("files must be provided in the YAML config")
+		return nil, fmt.Errorf("failed to unmarshal YAML content: %w", err)
 	}
 
 	return &config, nil
 }
 
-// Function to append parsed data for server level to the JSON file
-// func AppendServerParsedData(filePath string, keywords, sanitizationKeywords []string, parseAll bool, outputFilePath string) error {
-func AppendServerParsedData(filePath string, keywords, sanitizationKeywords []string, parseAll bool, outputFilePath string) error {
-	// Read the content of the file
-	fileContent, err := ioutil.ReadFile(filePath)
-	if err != nil {
-		return fmt.Errorf("failed to read file %q: %w", filePath, err)
-	}
+func appendParsedData(filePath string, parsedContent string, fileConfig FileConfig, outputFilePath string) error {
+	// Now sanitize the parsed content
+	sanitizedOutput := sanitizeOutput(parsedContent, fileConfig.SanitizationKeywords)
 
-	// Convert the content to a string
-	content := string(fileContent)
-
-	// Process all lines if parseAll is true and keywords is empty
-	if len(keywords) == 0 && parseAll {
-		// Create JSON data for the parsed file without parsing individual lines
-		jsonData := JSONData{
-			Command:     "File parsed: " + filePath,
-			Description: fmt.Sprintf("Keywords: %v, ParseAll: %t, SanitizationKeywords: %v", keywords, parseAll, sanitizationKeywords),
-			Output:      EncodeToBase64(content),
-			MonitorTag:  "pathtofile: " + filePath,
-		}
-
-		// Append the JSON data to the JSON file
-		err := AppendParsedDataToFile([]JSONData{jsonData}, outputFilePath)
-		if err != nil {
-			return fmt.Errorf("error appending instance JSON data to %s: %w", outputFilePath, err)
-		}
-
-		return nil
-	}
-
-	// Process the lines containing the keywords and append the JSON data
-	var outputLines []string
-	lines := strings.Split(content, "\n")
-	for _, line := range lines {
-		for _, keyword := range keywords {
-			if strings.Contains(line, keyword) {
-				outputLines = append(outputLines, line)
-				break // Move to the next line after processing the current keyword
-			}
-		}
-	}
-
-	// Sanitize the output before appending it to the file
-	sanitizedOutput := sanitizeOutput(strings.Join(outputLines, "\n"), sanitizationKeywords)
-
-	// Create JSON data for the parsed file
 	jsonData := JSONData{
 		Command:     "File parsed: " + filePath,
-		Description: fmt.Sprintf("File: %v, Keywords: %v, ParseAll: %t, SanitizationKeywords: %v", filePath, keywords, parseAll, sanitizationKeywords),
+		Description: fmt.Sprintf("File: %v", filePath),
 		Output:      EncodeToBase64(sanitizedOutput),
-		MonitorTag:  "pathtofile: " + filePath,
+		MonitorTag:  fileConfig.MonitorTag,
 	}
-
-	// Append the JSON data to the JSON file
-	err = AppendParsedDataToFile([]JSONData{jsonData}, outputFilePath)
-	if err != nil {
-		return fmt.Errorf("error appending server JSON data to %s: %w", outputFilePath, err)
-	}
-
-	return nil
-}
-
-// Function to append parsed data for instance level to the JSON file
-func AppendInstanceParsedData(filePath string, keywords, sanitizationKeywords []string, parseAll bool, outputFilePath, instanceArg string) error {
-	// Read the content of the file
-	fileContent, err := ioutil.ReadFile(filePath)
-	if err != nil {
-		return fmt.Errorf("failed to read file %q: %w", filePath, err)
-	}
-
-	// Convert the content to a string
-	content := string(fileContent)
-
-	// Process all lines if parseAll is true and keywords is empty
-	if len(keywords) == 0 && parseAll {
-		// Create JSON data for the parsed file without parsing individual lines
-		jsonData := JSONData{
-			Command:     "File parsed: " + filePath,
-			Description: fmt.Sprintf("Keywords: %v, ParseAll: %t, SanitizationKeywords: %v", keywords, parseAll, sanitizationKeywords),
-			Output:      EncodeToBase64(content),
-			MonitorTag:  "pathtofile: " + filePath,
-		}
-
-		// Append the JSON data to the JSON file
-		err := AppendParsedDataToFile([]JSONData{jsonData}, outputFilePath)
-		if err != nil {
-			return fmt.Errorf("error appending instance JSON data to %s: %w", outputFilePath, err)
-		}
-
-		return nil
-	}
-
-	// Process the lines containing the keywords and append the JSON data
-	var outputLines []string
-	lines := strings.Split(content, "\n")
-	for _, line := range lines {
-		for _, keyword := range keywords {
-			if strings.Contains(line, keyword) {
-				outputLines = append(outputLines, line)
-				break // Move to the next line after processing the current keyword
-			}
-		}
-	}
-
-	// Sanitize the output before appending it to the file
-	sanitizedOutput := sanitizeOutput(strings.Join(outputLines, "\n"), sanitizationKeywords)
-
-	// Create JSON data for the parsed file
-	jsonData := JSONData{
-		Command:     "File parsed: " + filePath,
-		Description: fmt.Sprintf("Keywords: %v, ParseAll: %t, SanitizationKeywords: %v", keywords, parseAll, sanitizationKeywords),
-		Output:      EncodeToBase64(sanitizedOutput),
-		MonitorTag:  "pathtofile: " + filePath,
-	}
-
-	// Append the JSON data to the JSON file
-	err = AppendParsedDataToFile([]JSONData{jsonData}, outputFilePath)
-	if err != nil {
-		return fmt.Errorf("error appending instance JSON data to %s: %w", outputFilePath, err)
-	}
-
-	return nil
+	return AppendParsedDataToFile([]JSONData{jsonData}, outputFilePath)
 }
